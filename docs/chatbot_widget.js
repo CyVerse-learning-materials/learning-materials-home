f8100d0e0105161edf04ce16cbc0d86f4494f70b
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2025-12-09 12:50:12 -0600

    Update chatbot_widget.js

--- docs/chatbot_widget.js
+++ docs/chatbot_widget.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
         <div id="chat-body">
             <iframe 
-                src="https://chat-qa.cyverse.org/learning/" 
+                src="https://cyversegpt.cyverse.org/" 
                 id="chat-frame" 
                 width="100%" 
                 height="100%"
